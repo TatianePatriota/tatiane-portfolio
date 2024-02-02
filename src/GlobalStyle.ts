f6ac79dcfc6f9ad172df1e874f9bbbbee750acda
Tatiane Patriota <tatianepatriota1@gmail.com>
2024-02-02 15:19:53 -0300

    fix global styles

---
+++ src/GlobalStyle.ts
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #04152d;
+    height: 100vh;
+    width: 100vw;
+    margin: 0;
+    color: #ffffff;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  li {
+    list-style: none;
+  }
+
+`;
