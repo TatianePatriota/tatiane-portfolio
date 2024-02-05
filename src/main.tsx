8441ab0d3ccbb367050882ed8fb0b43929782920
Tatiane Patriota <tatianepatriota1@gmail.com>
2024-02-05 16:13:33 -0300

    fix global styles and theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "styled-components";
-import { themes } from "./theme.ts";
+import { theme } from "./theme.ts";
+import { GlobalStyle } from "./GlobalStyle.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={themes}>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>
