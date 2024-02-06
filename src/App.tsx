86ae223628ab07ce579cb9cd58aaa61996e9f0cf
Tatiane Patriota <tatianepatriota1@gmail.com>
2024-02-06 17:06:44 -0300

    fix navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
-import { CssIcon } from "./components/Icons/CssIcon";
-import { HtmlIcon } from "./components/Icons/HtmlIcon";
-import { JavascriptIcon } from "./components/Icons/JavascriptIcon";
-import { ReactIcon } from "./components/Icons/ReactIcon";
-import { TypescriptIcon } from "./components/Icons/TypescriptIcon";
+import { NavBar } from "./components/Navbar/NavBar";
 
 function App() {
   return (
     <>
-      <div>
-        <JavascriptIcon />
-        <HtmlIcon />
-        <CssIcon />
-        <ReactIcon />
-        <TypescriptIcon />
-        <h2>Olá</h2>
-      </div>
+      <NavBar anchorText="Portfólio" />
     </>
   );
 }
